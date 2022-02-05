8ee5f965252e4119a53c42c93f3b37ac89219feb
Iuliia El Sammak <juliag875@gmail.com>
2022-02-05 14:50:06 -0500

    commit tree 5efed0af7eea16accd7d5c38be6c3c68a0826509

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    document.title = text;
+  }, [text]);
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 3000)
+  }, []);
+
   useEffect(() => {
     console.log("useEffect called");
-  });
+  }, [count]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount(()=> count+1)}>Click Me{count}</button>
+      <input type="text" value={text} placeholder="Type..."
+      onChange={(e)=> setText(e.target.value)} />
+    
+    <DogPics count={count} />
+    </div>
+    )
 }
 
 export default App;
--- src/DogPics.js
+++ src/DogPics.js
@@ -2,18 +2,19 @@ import React, { useState, useEffect } from "react";
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
-function DogPics() {
+function DogPics({count}) {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
     console.log("useEffect");
-    fetch("https://dog.ceo/api/breeds/image/random/3")
+    fetch("https://dog.ceo/api/breeds/image/random/1")
       .then((r) => r.json())
       .then((data) => {
         console.log("setState");
         setImages(data.message);
       });
-  });
+  }, {});
+// Component rendering-render-useEffect-useEffect called-setState-render
 
   console.log("render");
 
